ddbbe764092adde70f7c2ece246d456f7b23b7ce
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-08-17 06:37:24 -0300

    Add creative commons license

---
+++ Assets/Custom Assets/CustomUtility/Player/CharacterPoser.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+
+[RequireComponent(typeof(CharacterCustomization))]
+public class CharacterPoser : MonoBehaviour
+{
+    CharacterCustomization _customizator;
+    [SerializeField] bool track;
+    [SerializeField] Vector3 rootPosition;
+    [SerializeField] Quaternion head, body, leftShoulder, leftElbow, rightShoulder, rightElbow, leftLeg, leftKnee, leftFoot, rightLeg, rightKnee, rightFoot;
+
+    private void Awake() =>  _customizator = GetComponent<CharacterCustomization>();
+
+    private void Update()
+    {
+        if (track)
+        {
+            rootPosition = transform.position;
+            body = _customizator.baseBody.body.localRotation;
+            head = _customizator.baseBody.head.localRotation;
+            leftShoulder = _customizator.baseBody.leftShoulder.localRotation;
+            leftElbow = _customizator.baseBody.leftElbow.localRotation;
+            rightShoulder = _customizator.baseBody.rightShoulder.localRotation;
+            rightElbow = _customizator.baseBody.rightElbow.localRotation;
+            leftLeg = _customizator.baseBody.leftLeg.localRotation;
+            leftKnee = _customizator.baseBody.leftKnee.localRotation;
+            leftFoot = _customizator.baseBody.leftFoot.localRotation;
+            rightLeg = _customizator.baseBody.rightLeg.localRotation;
+            rightKnee = _customizator.baseBody.rightKnee.localRotation;
+            rightFoot = _customizator.baseBody.rightFoot.localRotation;
+        }
+    }
+    public void PoseCharacter()
+    {
+        transform.position = rootPosition;
+        _customizator.baseBody.body.localRotation = body;
+        _customizator.baseBody.head.localRotation = head;
+        _customizator.baseBody.leftShoulder.localRotation = leftShoulder;
+        _customizator.baseBody.leftElbow.localRotation = leftElbow;
+        _customizator.baseBody.rightShoulder.localRotation = rightShoulder;
+        _customizator.baseBody.rightElbow.localRotation = rightElbow;
+        _customizator.baseBody.leftLeg.localRotation = leftLeg;
+        _customizator.baseBody.leftKnee.localRotation = leftKnee;
+        _customizator.baseBody.leftFoot.localRotation = leftFoot;
+        _customizator.baseBody.rightLeg.localRotation = rightLeg;
+        _customizator.baseBody.rightKnee.localRotation = rightKnee;
+        _customizator.baseBody.rightFoot.localRotation = rightFoot;
+    }
+}
