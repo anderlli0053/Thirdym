5deece6d3b7c8049eaadfc26386878f00637ba67
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-09-15 21:37:31 -0300

    Update previous commit

--- Assets/Gann4Games/Scripts/Laser_Guide/LaserGuide.cs
+++ Assets/Gann4Games/Scripts/Laser_Guide/LaserGuide.cs
@@ -1,31 +1,34 @@
 ﻿using UnityEngine;
 
-public class LaserGuide : MonoBehaviour {
+namespace Gann4Games.Thirdym.Utility
+{
+    public class LaserGuide : MonoBehaviour {
 
-    LineRenderer _lineRenderer;
-    Vector3 _linePos;
+        LineRenderer _lineRenderer;
+        Vector3 _linePos;
 
-    public Transform hitPoint;
-    public float rayLenght;
+        public Transform hitPoint;
+        public float rayLenght;
 
-    void Start() {
-        _lineRenderer = GetComponent<LineRenderer>();
-    }
-    void Update()
-    {
-        RaycastHit hit;
-        rayLenght = Vector3.Distance(transform.position, hitPoint.position);
-        _linePos.z = rayLenght;
-        if(_lineRenderer != null)
-            _lineRenderer.SetPosition(1, _linePos);
-        if (Physics.Raycast(transform.position, transform.forward, out hit))
-        {
-            hitPoint.position = hit.point;
-            hitPoint.gameObject.SetActive(true);
+        void Start() {
+            _lineRenderer = GetComponent<LineRenderer>();
         }
-        else 
+        void Update()
         {
-            hitPoint.gameObject.SetActive(false);
+            RaycastHit hit;
+            rayLenght = Vector3.Distance(transform.position, hitPoint.position);
+            _linePos.z = rayLenght;
+            if(_lineRenderer != null)
+                _lineRenderer.SetPosition(1, _linePos);
+            if (Physics.Raycast(transform.position, transform.forward, out hit))
+            {
+                hitPoint.position = hit.point;
+                hitPoint.gameObject.SetActive(true);
+            }
+            else 
+            {
+                hitPoint.gameObject.SetActive(false);
+            }
         }
     }
 }
