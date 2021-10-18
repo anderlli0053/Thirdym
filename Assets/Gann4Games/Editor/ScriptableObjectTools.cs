0d496c80669ec17fafaf3e4e9d51e63755b65161
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-10-18 09:31:02 -0300

    Update ScriptableObjectTools.cs

--- Assets/Gann4Games/Editor/ScriptableObjectTools.cs
+++ Assets/Gann4Games/Editor/ScriptableObjectTools.cs
@@ -9,15 +9,22 @@ namespace Gann4Games.EditorTools
         /// <summary>
         /// Creates a new scriptable object of type specified type and stores the new created file into the scriptable objects folder.
         /// </summary>
-        /// <returns>The object that was created</returns>
+        /// <returns>The file that was created as an object</returns>
         public static object Create(Object scriptableObjectClass, string filename, string folder="")
         {
+        // Set the file name
             string fullFilename = filename + ".asset";
+
+        // Set the path
             string path = "Assets/Gann4Games/Resources/ScriptableObjects/" + folder + "/" + fullFilename;
 
+        // Create asset file
             AssetDatabase.CreateAsset(scriptableObjectClass, path);
 
+        // Show file in project window
             EditorUtility.FocusProjectWindow();
+            
+        // Select file to display its information in inspector
             Selection.activeObject = scriptableObjectClass;
 
             return scriptableObjectClass as ScriptableObject;
