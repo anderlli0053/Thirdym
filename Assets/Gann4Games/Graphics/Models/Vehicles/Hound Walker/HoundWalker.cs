ccddd8f8c7e610c17c3c10f9bb2056e8d050ac88
Santiago Ariel Mansilla <56666653+Gann4@users.noreply.github.com>
2021-10-18 22:29:19 -0300

    Reorganized files

---
+++ Assets/Gann4Games/Graphics/Models/Vehicles/Hound Walker/HoundWalker.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HoundWalker : MonoBehaviour {
+    
+    public Animator anim;
+    public float jointForce = 100000;
+    [HideInInspector] public ConfigurableJoint body;
+    HingeJoint[] limbs;
+
+    private void Start()
+    {
+        limbs = GetComponentsInChildren<HingeJoint>();
+        body = GetComponentInChildren<ConfigurableJoint>();
+    }
+    private void Update()
+    {
+        foreach (HingeJoint hj in limbs)
+        {
+            JointSpring limbSpring = hj.spring;
+            limbSpring.spring = jointForce;
+            hj.spring = limbSpring;
+        }
+    }
+}
