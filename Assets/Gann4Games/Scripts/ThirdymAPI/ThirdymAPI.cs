a792296e438eb4629193e7edc910c8384f419697
Santiago Ariel Mansilla <56666653+Gann4Life@users.noreply.github.com>
2022-06-17 22:06:33 -0300

    Update json serializationmethod
    For some reason newtonsoft's json serialization library has disappeared from the project.
    But unity has a built in class for this, so i used that insetad and works in a much simpler way.
    
    As an addition, version checking requests are done straight from the github repository.

---
+++ Assets/Gann4Games/Scripts/ThirdymAPI/ThirdymAPI.cs
@@ -0,0 +1,57 @@
+using System;
+using System.Collections;
+using UnityEngine;
+using UnityEngine.Networking;
+
+namespace Gann4Games
+{
+    public class ThirdymAPI
+    {
+
+        public event EventHandler OnRequestFinished;
+
+        public class GameCloudData
+        {
+            public string version;
+            public string download;
+        }
+
+        public string LastVersion => _gameCloudData.version;
+        public string DownloadLink => _gameCloudData.version;
+        public string CurrentVersion => Application.version;
+        public bool IsUpToDate => LastVersion == CurrentVersion;
+
+        string _dataURL = "https://raw.githubusercontent.com/Gann4Life/Thirdym/release/data.json";
+        string _json;
+        GameCloudData _gameCloudData;
+
+        public ThirdymAPI()
+        {
+            Debug.Log("API Initialized...");
+            OnRequestFinished += JsonToClass;
+        }
+
+        /// <summary>
+        /// Sends the json string data into `_gameCloudData` field.
+        /// </summary>
+        /// <param name="sender"></param>
+        /// <param name="args"></param>
+        void JsonToClass(object sender, EventArgs args) => _gameCloudData = JsonUtility.FromJson<GameCloudData>(_json);
+        
+        /// <summary>
+        /// Creates http request to save last game version into a json string for further use ingame.
+        /// </summary>
+        /// <returns></returns>
+        public IEnumerator InitializeRequest()
+        {
+            var request = UnityWebRequest.Get(_dataURL);
+            yield return request.SendWebRequest();
+            
+            if(request.result == UnityWebRequest.Result.Success)
+            {
+                _json = request.downloadHandler.text;
+                OnRequestFinished?.Invoke(this, EventArgs.Empty);
+            }
+        }
+    }
+}
